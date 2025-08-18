cbf93a7fcf4225d67c62d3ab6dd705964660a22a
mishtodo <todomikha@gmail.com>
2025-08-18 11:50:59 +0300

    backgrounds added, scripts changes

--- Platformer2D/Assets/Scripts/PlayerMovement.cs
+++ Platformer2D/Assets/Scripts/PlayerMovement.cs
@@ -6,12 +6,14 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float _speed = 5.0f;
     [SerializeField] private float _jumpForce = 10.0f;
     [SerializeField] private Rigidbody2D _rigidbody;
+    [SerializeField] private SpriteRenderer _spriteRenderer;
 
     private Vector2 _direction;
 
     private void Awake()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     private void Update()
@@ -23,5 +25,14 @@ public class PlayerMovement : MonoBehaviour
 
         _direction.x = Input.GetAxisRaw("Horizontal");
         _rigidbody.position += _direction * _speed * Time.deltaTime;
+
+        if (_direction.x < 0)
+        {
+            _spriteRenderer.flipX = true;
+        }
+        else if (_direction.x > 0)
+        {
+            _spriteRenderer.flipX = false;
+        }
     }
 }
