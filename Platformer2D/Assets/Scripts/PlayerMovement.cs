a8869461fb3afe8fe7424d342543c5c76ba53bab
mishtodo <todomikha@gmail.com>
2025-08-18 15:16:17 +0300

    animations added

--- Platformer2D/Assets/Scripts/PlayerMovement.cs
+++ Platformer2D/Assets/Scripts/PlayerMovement.cs
@@ -1,30 +1,39 @@
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Rigidbody2D), typeof(SpriteRenderer), typeof(Animator))]
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float _speed = 5.0f;
     [SerializeField] private float _jumpForce = 10.0f;
     [SerializeField] private Rigidbody2D _rigidbody;
     [SerializeField] private SpriteRenderer _spriteRenderer;
+    [SerializeField] private Animator _animator;
 
     private Vector2 _direction;
+    private bool _isGrounded;
 
     private void Awake()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
         _spriteRenderer = GetComponent<SpriteRenderer>();
+        _animator = GetComponent<Animator>();
     }
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && _isGrounded == true)
         {
-            _rigidbody.AddForce(Vector2.up * _jumpForce , ForceMode2D.Impulse);
+            _rigidbody.AddForce(Vector2.up * _jumpForce, ForceMode2D.Impulse);
+            _animator.Play("Jump");
         }
 
         _direction.x = Input.GetAxisRaw("Horizontal");
         _rigidbody.position += _direction * _speed * Time.deltaTime;
+
+        if (_direction.x != 0 && _isGrounded == true)
+        {
+            _animator.Play("Run");
+        }
 
         if (_direction.x < 0)
         {
@@ -34,5 +43,15 @@ public class PlayerMovement : MonoBehaviour
         {
             _spriteRenderer.flipX = false;
         }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        _isGrounded = true;
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        _isGrounded = false;
     }
 }
