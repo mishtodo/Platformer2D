59113ce9753c9db8c8722e1424e1cae6b23c06a6
mishtodo <130347522+mishtodo@users.noreply.github.com>
2025-11-04 10:47:17 +0300

    work in progress

---
+++ Platformer2D/Assets/Scripts/Player Scripts/PlayerHealth.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int maxHealth = 100;
+    public int currentHealth;
+
+    private void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        currentHealth = Mathf.Max(currentHealth, 0);
+
+        if (currentHealth <= 0)
+        {
+            //Destroy(gameObject);
+        }
+    }
+
+    public void Heal(int amount)
+    {
+        currentHealth += amount;
+        currentHealth = Mathf.Min(currentHealth, maxHealth);
+    }
+
+    private void Update()
+    {
+        
+    }
+}
